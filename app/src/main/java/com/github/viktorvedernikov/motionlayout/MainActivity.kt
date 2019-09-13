988837038319972d925a3e3cfa763795e951afad
Viktor Vedernikov <viktor.v@napoleonit.ru>
2019-09-13 16:30:10 +0500

    fix corned radius

--- app/src/main/java/com/github/viktorvedernikov/motionlayout/MainActivity.kt
+++ app/src/main/java/com/github/viktorvedernikov/motionlayout/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.github.viktorvedernikov.motionlayout
 
 import android.graphics.Color
+import android.graphics.Outline
 import android.os.Bundle
+import android.view.View
+import android.view.ViewOutlineProvider
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        topFrame.apply {
+            outlineProvider = object : ViewOutlineProvider() {
+                override fun getOutline(view: View?, outline: Outline?) {
+                    if (view != null && outline != null) {
+                        outline.setRoundRect(0, 0, view.width, view.height, dpToPx(20f).toFloat())
+                    }
+                }
+            }
+            clipToOutline = true
+        }
         motionLayout?.setBackgroundColor(Color.WHITE)
         motionLayout?.transitionToState(R.id.basketItemHidden)
         if (savedInstanceState == null) {
